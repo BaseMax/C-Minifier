7f62234761f59554be6fe1c65dee7c5ae13e411e
Max Base <maxbasecode@gmail.com>
2022-02-11 20:53:31 +0330

    start programming

--- Minifier.c
+++ Minifier.c
@@ -1,1 +1,12 @@
+/*
+ * Name: C Minifier
+ * Description: A C program to remove comments and minimize the code (remove whitespace and summarize code)
+ * Date: 2022-02-11
+ * Author: Max Base
+ * Repository: https://github.com/BaseMax/C-Minifier
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
